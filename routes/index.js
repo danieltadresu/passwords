3ad565a069134f05c573d7277a2a5dbf7e9332df
Dani <daniaxasabraxas@gmail.com>
2020-08-18 11:35:39 -0400

    se implementa funcion bcrypt

--- routes/index.js
+++ routes/index.js
@@ -16,49 +16,59 @@ router.post('/insert', function(req, res, next) {
   var username = req.body.formUserName;
   var secondname = req.body.formSecondName;
   var password = req.body.formPassword;
-  var bcrypt_salt_rounds = 12;
 
-  var user = new modelUser.user({
-    userName: username,
-    secondName: secondname,
-    password: password
+  var user = new modelUser.user();
+
+/*
+  bcrypt.hash(password, bcrypt.genSaltSync(5), function(err, hash) {
+    console.log('Password ORIGINAL - ' + password);
+    console.log('Password HASHEADA - ' + hash);
   });
-
-  console.log(user);
-  console.log('.........');
-
-  bcrypt.hash(password, bcrypt_salt_rounds, function(err, hash) {
-    console.log('PASSWORDS');
-    console.log('..........');
-    console.log('Clave Hasheada: ');
-    console.log(hash);
-    console.log('..........');
-    console.log('Clave original: ');
-    console.log(password);
-
-    user.save(function(err, myUserSaved) {
-      if (err) return console.error(err);
-      console.log('Saved!');
-      mongoose.disconnect();
-    });
-
-
-  });
-  res.redirect('/');
-
-});
-
-
-router.post('/consultar', function(req, res, next) {
-  var password = req.body.formPassword;
-
-  /* PENDIENTE, LA TAREA ES:
-
-    BUSCAR UN REGISTRO EN LA BASE DE DATOS CON LA CONTRASEÑA, DEVOLVERLO EN UN OBJETO Y COMPARARLO PARA USAR LAS CLAVES.
   */
 
+  user.userName = username;
+  user.secondName = secondname;
+  user.password = bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);
+
+
+  user.save(function(err, myUserSaved) {
+    if (err) return console.error(err);
+    console.log('Saved!');
+    console.log(user);
+
+  });
 
   res.redirect('/');
 });
+
+
+router.get('/consultar', function(req, res, next) {
+  res.render('search');
+});
+
+
+router.post('/select', function(req, res, next) {
+  var userName = req.body.formUserName;
+  var userPassword = req.body.formUserPassword;
+  console.log(userName);
+
+
+  modelUser.user.findOne({'userName': userName}, function(err, user){
+    console.log(user);
+
+    var check = bcrypt.compareSync(userPassword, user.password);
+
+    if(check) {
+      console.log('Son correctas!');
+    }
+  });
+
+
+
+
+  res.redirect('/');
+});
+
+
 
 module.exports = router;
